40b3e3edf40b2ed376c769ca54ba669d3ab94e8d
Manuel <105855842+Gurrupato@users.noreply.github.com>
2022-05-29 18:18:03 +0200

    Counting Repeats

--- ejercicios extra.js
+++ ejercicios extra.js
@@ -1,15 +1,7 @@
-
-
-
-array= [1,1,3,4,1,4,2,3]
-export const countRepeats = (array) => {
-     let results = [];
-     for (let j = 0; j <array.length; j++){
-         for (let i = j+1; i <array.length; i++){
-            if (array[j] ==array[i]){
-             results.push(array[i:+1]);
-         }
-        }
-     }
-  return [...new Set(results)];
- }; 
+array = [5, 5, 5, 2, 2, 2, 2, 2, 9, 4];
+result = {};
+for (var i = 0; i < array.length; ++i) {
+  if (!result[array[i]]) result[array[i]] = 0;
+  ++result[array[i]];
+}
+console.log(result);
